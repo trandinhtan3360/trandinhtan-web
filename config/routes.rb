f5cabfadfa659963d6b15b2b4596ad1ce7a88b85
Tran Dinh Tan <trandinhtan.web@gmail.com>
2017-06-06 14:22:14 +0700

    kk

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'users#new'
   post '/signup', to: 'users#create'
-
+  match '/signup',  to: 'users#new',             via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-
   delete '/logout',  to: 'sessions#destroy'
-
   resources :microposts
   resources :users
   root 'users#index'
