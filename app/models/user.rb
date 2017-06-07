6d3d01af50fabd5a51c5eeae57c09ba0b73ac16f
Tran Dinh Tan <trandinhtan.web@gmail.com>
2017-06-07 11:44:24 +0700

    account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
 	has_many :microposts
   #validates name, presence: true    # Replace FILL_IN with the right code.
   #validates email, presence: true    # Replace FILL_IN with the right code.
-   attr_accessor :remember_token
+   attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
    before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -37,5 +39,16 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+# Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
