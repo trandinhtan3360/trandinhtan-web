2060b7e5edabb5cf7d9b2b47e261ddec2eb902e7
Tran Dinh Tan <trandinhtan.web@gmail.com>
2017-06-08 09:27:31 +0700

    activation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,9 +33,11 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end  
 
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end 
+  def authenticated?(attribute, token)
+  digest = self.send("#{attribute}_digest")
+  return false if digest.nil?
+  BCrypt::Password.new(digest).is_password?(token)
+end
 
   def forget
     update_attribute(:remember_digest, nil)
@@ -53,7 +55,7 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
